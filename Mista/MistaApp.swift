6202ac5a38fb8b687bea58f9e7346506fa31dde3
AvishkaRavishan <avishk678@icloud.com>
2023-06-19 23:56:44 +0530

    Initial Commit

---
+++ Mista/MistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MistaApp.swift
+//  Mista
+//
+//  Created by AVISHKA RAVISHAN on 2023-06-19.
+//
+
+import SwiftUI
+
+@main
+struct MistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
