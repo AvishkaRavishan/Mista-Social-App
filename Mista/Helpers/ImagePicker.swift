5a7dbf7c96f5c013c4a56b2fd656fa288bf28031
AvishkaRavishan <avishk678@icloud.com>
2023-06-21 13:27:57 +0530

    updste

--- Mista/Helpers/ImagePicker.swift
+++ Mista/Helpers/ImagePicker.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedImage: UIImage?
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        @Binding var presentationMode: PresentationMode
+        @Binding var selectedImage: UIImage?
+
+        init(presentationMode: Binding<PresentationMode>, selectedImage: Binding<UIImage?>) {
+            _presentationMode = presentationMode
+            _selectedImage = selectedImage
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController,
+                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                selectedImage = image
+            }
+            presentationMode.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            presentationMode.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(presentationMode: presentationMode, selectedImage: $selectedImage)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .photoLibrary
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController,
+                                context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+}
